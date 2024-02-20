e2e6ab87970fdeb03a16c4f9d99a306524064bbd
hung10t3 <hung10t@gmail.com>
2024-02-20 23:00:07 +0700

    Add implement product images

--- script.js
+++ script.js
@@ -57,3 +57,28 @@ const dptClass = document.querySelector(".site");
 dptButton.addEventListener('click', () => {
     dptClass.classList.toggle("showdpt");
 })
+// product image slider
+var productThumb = new Swiper('.small-image', {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 3,
+    freeMode: true,
+    // watchSlidesProgress: true,
+    breakpoints: {
+        481: {
+            spaceBetween: 32,
+        }
+    }
+});
+
+var productBig = new Swiper('.big-image', {
+    loop: true,
+    autoHeight: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+        swiper: productThumb
+    }
+})
